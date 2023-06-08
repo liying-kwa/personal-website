f7eedf6c68f9a15482b013687965e7a4c4b470c2
Li Ying Kwa <liying@Liying-MacBook-Pro.local>
2023-06-08 20:06:59 +0800

    Implement ProjectCard as a reusable component

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,13 +1,20 @@
 import { Quicksand } from 'next/font/google'
+import ProjectCard from '@/components/ProjectCard'
 
+// TODO: Choose font(s) for ProjectCard
 const quicksand = Quicksand({ subsets: ['latin'] })
 
 const Projects = () => {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white dark:bg-black text-black dark:text-white">
-            <div className="font-mono">Projects</div>
-            <div className={`${quicksand.className}`}>Projects</div>
-            <div>Projects</div>
+        <main className="flex flex-col min-h-screen items-center justify-between p-24 bg-white dark:bg-black">
+            <ProjectCard
+                title="Git Duet"
+                description="Implemented the 'ALLOW_MULTIPLE_COMMITTERS' Feature"
+                date="Nov 2022"
+                flipDescriptionLinks={{ repository: 'https://github.com/git-duet/git-duet', feature: 'https://github.com/git-duet/git-duet/pull/118' }}
+                flipDescription={["Inspiration: blah blah blah", "Additional comments: blah blah blah"]}
+                imgSrc="/git-duet.png"
+                imgAlt="git-duet" />
         </main>
     )
 }
